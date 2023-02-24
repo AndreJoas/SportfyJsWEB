b5cf8e2bfbad8787f1e2bb80c9a4cbcd9ad2d6c0
unknown <andrejoas93@gmail.com>
2023-02-24 20:42:46 -0300

    sportyMusic

--- funcoes/page02.js
+++ funcoes/page02.js
@@ -6,23 +6,14 @@ var fundoLogin = document.getElementById("loginP");
 var fundoFormulario = document.getElementById("cabeca");
 
 // ----------cards----
-var carde = document.getElementById("cardMusica1");
-var carde2 = document.getElementById("cardMusica2");
-var carde3 = document.getElementById("cardMusica3");
-var carde4 = document.getElementById("cardMusica4");
-var carde5 = document.getElementById("cardMusica5");
-var carde6 = document.getElementById("cardMusic61");
+var carde = document.getElementById("listaMusicas");
+
 // ---------
 
 function Noite() {
+  
+    carde.style.border= "solid 2px #45e01e";
     fundoLogin.style.border = "solid 2px #45e01e";
-    carde.style.border = "solid 2px #45e01e";
-    carde2.style.border = "solid 2px #45e01e";
-    carde3.style.border = "solid 2px #45e01e";
-    carde4.style.border = "solid 2px #45e01e";
-    carde5.style.border = "solid 2px #45e01e";
- 
-
     fundoFormulario.style.border = "solid 2px #45e01e";
     fundoFormulario.style.background = "linear-gradient(90deg,#097927 26%,#010a0d 96%)";
     fundoFormulario.style.color = "#000000";
@@ -32,6 +23,7 @@ function Noite() {
 }
 
 function Dia() {
+    carde.style.border= "solid 2px #000000";
     fundoLogin.style.border = "solid 2px #000000";
     fundoFormulario.style.border = "solid 3px #000000";
     fundoCorpo.style.background = "radial-gradient(circle, rgba(9,121,39,1) 10%, rgba(19,47,27,1) 65%)";
--- pages/pag2/funçoes.js
+++ pages/pag2/funçoes.js
@@ -1,13 +1,33 @@
 // funcao que alem de ler o conteudo de uma pagina html em outra, so retorna a div com a classe .ret1
 
+// musicas MICHAEL JACKSON
 const card=$(document).ready(function(){
-    $("#card1").click(function(){
+    $(".card1").click(function(){
         $(function(){
-            $("#cardMusica1").load("musicas.html .ftautor,.infor, .coracao");
+            $("#listaMusicas").load("musicas.html #cardMusica1, #cardMusica2, #cardMusica3, #cardMusica4, #cardMusica5");
         });
     })
 });
 
+
+const card2=$(document).ready(function(){
+    $(".card2").click(function(){
+        $(function(){
+            $("#listaMusicas").load("musicas.html #cardMusica6, #cardMusica7, #cardMusica8, #cardMusica9, #cardMusica10");
+        });
+    })
+});
+
+
+
+
 function Skiller(){
-    document.getElementById("cardMusica1").innerHTML=card;
+    document.getElementById("listaMusicas").innerHTML=card;
+
+}
+
+
+function Marilia(){
+    document.getElementById("listaMusicas").innerHTML=card2;
+
 }
